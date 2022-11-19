d2553aac06fc9e59553a0661a50a3a46d735a19c
chrisdadev13 <chrisdadev13@gmail.com>
2022-11-18 21:00:28 -0400

    card component created and code cleaning

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -16,6 +16,7 @@ import {
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./Navbar";
 import Homepage from "./pages/Homepage";
+import ProductCard from "./utils/Card";
 
 const Main = () => {
   const [men, setMen] = useState([]);
@@ -62,8 +63,10 @@ const Main = () => {
     if (alreadyOnCart(cart, product)) {
       let direction = cart.findIndex((item) => item.id === product.id);
       cart[direction].quantity += 1;
+      setTotal(total + cart[direction].price * cart[direction].quantity);
     } else {
       setCart([...cart, product]);
+      setTotal(total + product.price);
     }
   };
 
@@ -84,45 +87,14 @@ const Main = () => {
         gap="2"
       >
         {data.map((product, index) => (
-          <Card maxW="90%" maxH="7xl" shadow="lg" mx="2" key={index}>
-            <CardBody>
-              <Image
-                src={product.image}
-                alt={product.title}
-                borderRadius="lg"
-                w={{ "2xl": "52", xl: "36", lg: "36", md: "36", sm: "44" }}
-              />
-              <Stack mt="6" spacing="3">
-                <Heading size={{ "2xl": "md", xl: "sm" }}>
-                  {product.title}
-                </Heading>
-                <Text
-                  noOfLines={[1, 2]}
-                  textTransform="capitalize"
-                  size={{ xl: "16" }}
-                >
-                  {product.description}
-                </Text>
-                <Text color="blue.600">${product.price}</Text>
-              </Stack>
-            </CardBody>
-            <Divider />
-            <CardFooter>
-              <ButtonGroup spacing="2">
-                <Button
-                  variant="ghost"
-                  colorScheme="blue"
-                  key={index}
-                  data={index}
-                  onClick={addToCart}
-                >
-                  Add to cart
-                </Button>
-              </ButtonGroup>
-            </CardFooter>
-          </Card>
+          <ProductCard
+            product={product}
+            index={index}
+            onCardClick={addToCart}
+          />
         ))}
       </Grid>
+      <p style={{ marginLeft: "20px" }}>{Math.trunc(total)}</p>
     </Box>
   );
 };

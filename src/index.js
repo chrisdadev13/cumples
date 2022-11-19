f70d8b8a67d383c8c5e0cbc6867ddc1457475f4e
chrisdadev13 <chrisdadev13@gmail.com>
2022-11-19 19:35:17 -0400

    redux

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <ChakraProvider>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>,
